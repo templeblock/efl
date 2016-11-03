70cd22bf0760ccaedfb074c1b0ca0ed248307edf
Daniel Hirt <daniel.hirt@samsung.com>
2016-11-03 17:45:42 +0200

    Edje containers: split logic

---
+++ src/lib/edje/edje_part.c
@@ -0,0 +1,14 @@
+#include "edje_private.h"
+#include "edje_part_helper.h"
+
+PROXY_INIT(box)
+PROXY_INIT(table)
+PROXY_INIT(swallow)
+
+void
+_edje_internal_proxy_shutdown(void)
+{
+   _box_shutdown();
+   _table_shutdown();
+   _swallow_shutdown();
+}
